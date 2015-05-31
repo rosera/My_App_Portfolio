3abc60f32284255617cc1047dc1bcee809879c84
rosera <technologynorth@gmail.com>
2015-05-31 15:24:49 +0100

    Reorganise code locations for Udacity project structure.

--- app/src/main/java/com/texturelabs/rosera/myappportfolio/MainActivity.java
+++ app/src/main/java/com/texturelabs/rosera/myappportfolio/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         setContentView(R.layout.app_portfolio);
     }
 
